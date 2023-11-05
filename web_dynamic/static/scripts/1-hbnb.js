20f519475b5353f5f99d0b1347a21d2fb8695724
Chura33 <churabrady@gmail.com>
2023-11-05 20:39:14 +0100

    added recent features

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,12 @@
+$( document ).ready(function (){
+    let ls_amen = [];
+    $('input[type=checkbox]').change (function (){
+        let name = $(this).attr('data-name');
+        if ($(this).is(':checked')){
+            ls_amen.push(name);
+        } else {
+            ls_amen = ls_amen.filter(amen => amen != name);
+        }
+        $('.amenities h4').text(ls_amen.join(', '));
+    });
+});
